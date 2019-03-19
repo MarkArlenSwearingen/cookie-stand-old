a76a710e9c04158612535535576974941e4bd2ab
Mark Swearingen <45861248+Swearingenius@users.noreply.github.com>
2019-03-19 13:17:22 -0700

    Added multiple stores, used back quote, linked from sales back to index.html

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ var firstAndPike = {
     // this.cookiesByHour.push('TEST');
     for(var i = 0; i < this.hours.length; i++){
       var simulatedNumberOfCustomersPerHour = (Math.ceil((Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers));
-      console.log(simulatedNumberOfCustomersPerHour);
+      //console.log(simulatedNumberOfCustomersPerHour);
       var cookiesByHour = Math.ceil(simulatedNumberOfCustomersPerHour * this.avgCookiesPerCustomer);
       firstAndPike.cookiesByHour.push(cookiesByHour);
       //console.log(cookiesByHour);
@@ -33,20 +33,195 @@ var ul = document.getElementById('cookieForecast');
 
 for(var i = 0; i < firstAndPike.hours.length; i++){
   var li = document.createElement('li');
-  li.textContent =  firstAndPike.hours[i] + ' ' +firstAndPike.cookiesByHour[i] + ' cookies';
+  li.textContent =  `${firstAndPike.hours[i]} ${firstAndPike.cookiesByHour[i]} cookies`;
   ul.appendChild(li);
   totalCookiesByStore = totalCookiesByStore + firstAndPike.cookiesByHour[i];
 
 }
 var liTotalText = 'Total: ' + totalCookiesByStore + ' cookies';
-var li = document.createElement('li');
+li = document.createElement('li');
 li.textContent = (liTotalText);
 ul.appendChild(li);
 
-console.log(totalCookiesByStore);
+//variable to keep  information about SeaTac airport
+var seaTac = {
+  storeLocation: 'SeaTac',
+  minHourlyCustomers: 3,
+  maxHourlyCustomers: 24,
+  avgCookiesPerCustomer: 1.2,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  cookiesByHour: [],
+  calculateAvgCookies: function(){
+    // this.cookiesByHour.push('TEST');
+    for(var i = 0; i < this.hours.length; i++){
+      var simulatedNumberOfCustomersPerHour = (Math.ceil((Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers));
+      //console.log(simulatedNumberOfCustomersPerHour);
+      var cookiesByHour = Math.ceil(simulatedNumberOfCustomersPerHour * this.avgCookiesPerCustomer);
+      seaTac.cookiesByHour.push(cookiesByHour);
+      //console.log(cookiesByHour);
+      //console.log(simulatedNumberOfCustomersPerHour);
+    }
+  },
+};
 
+allStores.push(seaTac);
+seaTac.calculateAvgCookies();
+totalCookiesByStore = 0;
 
+//console.log(totalCookiesByStore);
+//console.log(allStores);
 
+ul = document.getElementById('cookieForecastSeatac');
+
+for( i = 0; i < seaTac.hours.length; i++){
+  li = document.createElement('li');
+  li.textContent =  `${firstAndPike.hours[i]} ${firstAndPike.cookiesByHour[i]} cookies`;
+  ul.appendChild(li);
+  totalCookiesByStore = totalCookiesByStore + seaTac.cookiesByHour[i];
+
+}
+liTotalText = 'Total: ' + totalCookiesByStore + ' cookies';
+li = document.createElement('li');
+li.textContent = (liTotalText);
+ul.appendChild(li);
+
+//variable to keep  information about Seattle Center
+var seattleCenter = {
+  storeLocation: 'seattleCenter',
+  minHourlyCustomers: 11,
+  maxHourlyCustomers: 38,
+  avgCookiesPerCustomer: 3.7,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  cookiesByHour: [],
+  calculateAvgCookies: function(){
+    // this.cookiesByHour.push('TEST');
+    for(var i = 0; i < this.hours.length; i++){
+      var simulatedNumberOfCustomersPerHour = (Math.ceil((Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers));
+      // console.log(simulatedNumberOfCustomersPerHour);
+      var cookiesByHour = Math.ceil(simulatedNumberOfCustomersPerHour * this.avgCookiesPerCustomer);
+      seattleCenter.cookiesByHour.push(cookiesByHour);
+      //console.log(cookiesByHour);
+      //console.log(simulatedNumberOfCustomersPerHour);
+    }
+  },
+};
+
+allStores.push(seattleCenter);
+seattleCenter.calculateAvgCookies();
+totalCookiesByStore = 0;
+
+// console.log(totalCookiesByStore);
+// console.log(allStores);
+
+ul = document.getElementById('cookieForecastSeattleCenter');
+
+for( i = 0; i < seattleCenter.hours.length; i++){
+  li = document.createElement('li');
+  li.textContent =  `${firstAndPike.hours[i]} ${firstAndPike.cookiesByHour[i]} cookies`;
+  ul.appendChild(li);
+  totalCookiesByStore = totalCookiesByStore + seattleCenter.cookiesByHour[i];
+
+}
+liTotalText = 'Total: ' + totalCookiesByStore + ' cookies';
+li = document.createElement('li');
+li.textContent = (liTotalText);
+ul.appendChild(li);
+
+// console.log(totalCookiesByStore);
+// console.log(allStores);
+
+//variable to keep  information about Capitol Hill
+var capitolHill = {
+  storeLocation: 'capitolHill',
+  minHourlyCustomers: 20,
+  maxHourlyCustomers: 38,
+  avgCookiesPerCustomer: 2.3,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  cookiesByHour: [],
+  calculateAvgCookies: function(){
+    // this.cookiesByHour.push('TEST');
+    for(var i = 0; i < this.hours.length; i++){
+      var simulatedNumberOfCustomersPerHour = (Math.ceil((Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers));
+      // console.log(simulatedNumberOfCustomersPerHour);
+      var cookiesByHour = Math.ceil(simulatedNumberOfCustomersPerHour * this.avgCookiesPerCustomer);
+      capitolHill.cookiesByHour.push(cookiesByHour);
+      //console.log(cookiesByHour);
+      //console.log(simulatedNumberOfCustomersPerHour);
+    }
+  },
+};
+
+allStores.push(capitolHill);
+capitolHill.calculateAvgCookies();
+totalCookiesByStore = 0;
+
+// console.log(totalCookiesByStore);
+// console.log(allStores);
+
+ul = document.getElementById('cookieForecastCapitolHill');
+
+for( i = 0; i < capitolHill.hours.length; i++){
+  li = document.createElement('li');
+  li.textContent =  `${firstAndPike.hours[i]} ${firstAndPike.cookiesByHour[i]} cookies`;
+  ul.appendChild(li);
+  totalCookiesByStore = totalCookiesByStore + capitolHill.cookiesByHour[i];
+
+}
+liTotalText = 'Total: ' + totalCookiesByStore + ' cookies';
+li = document.createElement('li');
+li.textContent = (liTotalText);
+ul.appendChild(li);
+
+// console.log(totalCookiesByStore);
+// console.log(allStores);
+
+//variable to keep  information about Alki
+var alki = {
+  storeLocation: 'alki  ',
+  minHourlyCustomers: 20,
+  maxHourlyCustomers: 38,
+  avgCookiesPerCustomer: 2.3,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  cookiesByHour: [],
+  calculateAvgCookies: function(){
+    // this.cookiesByHour.push('TEST');
+    for(var i = 0; i < this.hours.length; i++){
+      var simulatedNumberOfCustomersPerHour = (Math.ceil((Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers)) + this.minHourlyCustomers));
+      // console.log(simulatedNumberOfCustomersPerHour);
+      var cookiesByHour = Math.ceil(simulatedNumberOfCustomersPerHour * this.avgCookiesPerCustomer);
+      alki.cookiesByHour.push(cookiesByHour);
+      //console.log(cookiesByHour);
+      //console.log(simulatedNumberOfCustomersPerHour);
+    }
+  },
+};
+
+allStores.push(alki);
+alki.calculateAvgCookies();
+totalCookiesByStore = 0;
+
+// console.log(totalCookiesByStore);
+// console.log(allStores);
+
+ul = document.getElementById('cookieForecastAlki');
+
+for( i = 0; i < alki.hours.length; i++){
+  li = document.createElement('li');
+  li.textContent =  `${firstAndPike.hours[i]} ${firstAndPike.cookiesByHour[i]} cookies`;
+  ul.appendChild(li);
+  totalCookiesByStore = totalCookiesByStore + alki.cookiesByHour[i];
+
+}
+liTotalText = 'Total: ' + totalCookiesByStore + ' cookies';
+li = document.createElement('li');
+li.textContent = (liTotalText);
+ul.appendChild(li);
+
+// console.log(totalCookiesByStore);
+console.log(allStores);
+// Store the results for each location in a separate array... perhaps as a property of the object representing that location
+
+// Display the values of each array as unordered lists in the browser
 
 
 
---
+++ sandbox/randomnumbergenerator/app.js
@@ -0,0 +1,8 @@
+var min =23 ;
+var max = 65;
+
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+
+var foo = String('This is my string as an object');
+console.log(foo);
