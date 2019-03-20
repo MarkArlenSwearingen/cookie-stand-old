219ab99ba65b089ae21af483bdfc0e1cc43e68be
Mark Swearingen <45861248+Swearingenius@users.noreply.github.com>
2019-03-20 16:58:34 -0700

    Removing comments, unused code and adding Define Sections

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@
 // Define all global functions to be used in script
 // --------------------------------
 
-var allStores = [];
-var HOURS = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 
 
 //variable to keep  information about firstAndPike
@@ -25,15 +23,22 @@ Store.prototype.calculateAvgCookies = function(){
     var result = Math.ceil(simulatedNumberOfCustomersPerHour * this.avgCookiesPerCustomer);
     this.cookiesByHour.push(result);
   }
-
 };
 
-
+// --------------------------------
+// Define all data to be used in script
+// --------------------------------
+var allStores = [];
+var HOURS = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
+var totalCookiesByStore = 0;
+// --------------------------------
+// Run script
+// --------------------------------
 var firstAndPike = new Store('1st and Pike', 23, 65, 6.3); //creates store
 
 allStores.push(firstAndPike);
 firstAndPike.calculateAvgCookies();
-var totalCookiesByStore = 0;
+
 
 // gets parent table for creating elements within table
 var parentTable = document.getElementById('storeTable'); // Step 1: Get Parent Element
@@ -54,22 +59,21 @@ console.log(tr);
 
 //set global th so "var =" is not in function
 var th;
+
+//funciton to create th elements, text and attributes with values
 function createth(thContent, parentElement, attributes, value){
   th = document.createElement('th');
   if (thContent){
     th.textContent=thContent;
   }
   if (attributes){
-    th.setAttribute(attributes, value);}
+    th.setAttribute(attributes, value);
+  }
   parentElement.appendChild(th);
 }
 
-// var th = document.createElement('th'); // create th element
-// th.textContent='Cookie Forecast'; // give element content
-// tr.appendChild(th);// append to tr element
-// console.log(th);
 createth('Cookie Forecast', tr, null, null); 
-createth('Second Header',tr, null, null);
+//createth('Second Header',tr, null, null);
 
 var tbody = document.createElement('tbody'); // create tbody element
 parentTable.appendChild(tbody);// append to table element
@@ -78,8 +82,6 @@ console.log(tbody);
 tr = document.createElement('tr'); //create tr element
 tbody.appendChild(tr);// Append tr to the tbody
 console.log(tr);
-
-
 
 createth(null,tr);
 
@@ -92,35 +94,20 @@ tr = document.createElement('tr'); //create tr element
 tbody.appendChild(tr);// Append tr to the tbody
 createth('1st and Pike', tr, 'scope', 'row');
 
+//for loop to create th elements including number of cookies by hour
 for(i=0; i < firstAndPike.cookiesByHour.length; i++){
   th = document.createElement('th');// create td
   th.textContent=firstAndPike.cookiesByHour[i]; // add content
   tr.appendChild(th);// add td to tr 
 }
 
-//End of creating data for first and Pike - Refactor End for th
+//End of creating data for first and Pike 
 
 // Create tr for seaTac
 tr = document.createElement('tr'); //create tr element
 tbody.appendChild(tr);// Append tr to the tbody
 console.log(tr);
 createth('seaTac', tr, 'scope', 'row');
-
-// for(i=0; i < seaTac.cookiesByHour.length; i++){
-//   th = document.createElement('th');// create td
-//   th.textContent=seaTac.cookiesByHour[i]; // add content
-//   tr.appendChild(th);// add td to tr 
-// }
-
-// tr = document.createElement('tr'); //create tr element
-// tbody.appendChild(tr);// Append tr to the tbody
-// console.log(tr);
-// createth('Capitol Hill', tr, 'scope', 'row');
-
-// tr = document.createElement('tr'); //create tr element
-// tbody.appendChild(tr);// Append tr to the tbody
-// console.log(tr);
-// createth('Alki', tr, 'scope', 'row');
 
 //variable to keep information about SeaTac airport
 
@@ -135,8 +122,6 @@ for(i=0; i < seaTac.cookiesByHour.length; i++){
   th.textContent=seaTac.cookiesByHour[i]; // add content
   tr.appendChild(th);// add td to tr 
 }
-
-var ul = document.getElementById('cookieForecastSeatac');
 
 //variable to keep  information about Seattle Center
 
@@ -157,20 +142,6 @@ for(i=0; i < seattleCenter.cookiesByHour.length; i++){
   tr.appendChild(th);// add td to tr 
 }
 
-ul = document.getElementById('cookieForecastSeattleCenter');
-
-// for( i = 0; i < seattleCenter.hours.length; i++){
-//   li = document.createElement('li');
-//   li.textContent =  `${firstAndPike.hours[i]} ${firstAndPike.cookiesByHour[i]} cookies`;
-//   ul.appendChild(li);
-//   totalCookiesByStore = totalCookiesByStore + seattleCenter.cookiesByHour[i];
-
-// }
-// liTotalText = 'Total: ' + totalCookiesByStore + ' cookies';
-// li = document.createElement('li');
-// li.textContent = (liTotalText);
-// ul.appendChild(li);
-
 //variable to keep  information about Capitol Hill
 
 var CapitolHill = new Store('Capitol Hill', 20, 38, 2.3);
@@ -190,28 +161,9 @@ for(i=0; i < CapitolHill.cookiesByHour.length; i++){
   tr.appendChild(th);// add td to tr 
 }
 
-ul = document.getElementById('cookieForecastCapitolHill');
-
-// for( i = 0; i < CapitolHill.hours.length; i++){
-//   li = document.createElement('li');
-//   li.textContent =  `${firstAndPike.hours[i]} ${firstAndPike.cookiesByHour[i]} cookies`;
-//   ul.appendChild(li);
-//   totalCookiesByStore = totalCookiesByStore + CapitolHill.cookiesByHour[i];
-
-// }
-// liTotalText = 'Total: ' + totalCookiesByStore + ' cookies';
-// li = document.createElement('li');
-// li.textContent = (liTotalText);
-// ul.appendChild(li);
-
-// console.log(totalCookiesByStore);
-// console.log(allStores);
-
 //variable to keep  information about Alki
 
 var Alki = new Store('Alki', 2, 16, 4.6);
-
-
 
 allStores.push(Alki);
 Alki.calculateAvgCookies();
@@ -228,13 +180,13 @@ for(i=0; i < Alki.cookiesByHour.length; i++){
   tr.appendChild(th);// add td to tr 
 }
 
-ul = document.getElementById('cookieForecastAlki');
+var firstAndPikeTotal = 0;
+for( i = 0; i < firstAndPike.cookiesByHour.length; i++){
+  firstAndPikeTotal = firstAndPikeTotal + firstAndPike.cookiesByHour[i];
+}
 
-// for( i = 0; i < alki.hours.length; i++){
-//   li = document.createElement('li');
-//   li.textContent =  `${firstAndPike.hours[i]} ${firstAndPike.cookiesByHour[i]} cookies`;
-//   ul.appendChild(li);
-//   totalCookiesByStore = totalCookiesByStore + alki.cookiesByHour[i];
+console.log(firstAndPike.cookiesByHour);
+console.log(firstAndPikeTotal);
 
 // }
 // liTotalText = 'Total: ' + totalCookiesByStore + ' cookies';
@@ -244,18 +196,3 @@ ul = document.getElementById('cookieForecastAlki');
 
 // console.log(totalCookiesByStore);
 console.log(allStores);
-
-
-//get  the parent element
-// var store = document.getElementById('pikeStreet');
-// console.log(store);
-
-// //Create element
-// var li = document.createElement('li'); // 2. Create element
-
-// //Give element content
-// li.textContent = firstAndPike.cookiesByHour; // 3. Give element content
-
-// //Append to the document.
-// ul.appendChild(li); // 4. Append to the document
-
