cb4f9e487a48ed1e3a086b5cf8e0639aef0f0c65
Mark Swearingen <45861248+Swearingenius@users.noreply.github.com>
2019-03-25 19:02:44 -0700

    Removed console.log code

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ function Store(location, store, minCustomer, maxCustomer, avgCookies, cookiesByH
 }
 
 Store.prototype.randomNumberOfCustomerPerHour = function(){
-  // return Math.ceil((Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
 };
 
 Store.prototype.render = function() {
@@ -26,7 +25,6 @@ Store.prototype.render = function() {
   th.setAttribute('scope', 'row'); //provide content
   th.textContent = this.location; //provide content
   tr.appendChild(th);//attach element to parent
-  console.log('render function this.cookiesByHour ' + this.cookiesByHour );
   hourlyTotalForAllLocations.push(this.cookiesByHour);
   
   for(i=0; i < HOURS.length; i++){
@@ -78,6 +76,18 @@ var createTableHeader = function(){
   }
 };
 
+//Calculate Hourly Totals
+var calculateHourlyTotals = function(){
+  for(i=1; i< HOURS.length + 2; i++){
+    if (i > 1 ){cookiesByHour.push(hourSum);
+    }
+    hourSum = 0;
+    for (j=1; j < hourlyTotalForAllLocations.length + 1; j++){
+      hourTotal = document.getElementById("storeTable").rows[j].cells[i].innerHTML
+      hourSum = hourTotal * 1 + hourSum;
+    }
+  }
+};
 //Table footer function
 var createTableFooter = function(){
   //<table id="storeTable">
@@ -94,10 +104,8 @@ var createTableFooter = function(){
   // Loop to create column labels
   for (i=0; i < HOURS.length; i++){
     th = document.createElement('th'); //create element
-    console.log(cookiesByHour);
     hourTotal = cookiesByHour[i];
     th.textContent = hourTotal; //provide content
-    console.log(th.textContent);
     th.setAttribute('scope', 'col'); //provide content
     tr.appendChild(th);//attach element to parent
     if (i === HOURS.length -1){
@@ -113,7 +121,6 @@ var createTableFooter = function(){
 var HOURS = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 var totalCookiesByStore = 0;
 var cookiesByHour = [];
-var cookiesByStore = 0;
 var hourlyTotalForAllLocations = [];
 var grandTotal= 0;
 var hourTotal = 0;
@@ -124,7 +131,7 @@ var tbody = document.createElement('tbody');
 // --------------------------------
 // Run script
 // --------------------------------
-storeTable.appendChild(tbody);//attach tbody element to parent element to prepare DOM for rendering
+storeTable.appendChild(tbody);//attach tbody element to parent table element to prepare DOM for rendering
 
 // Objects to keep information by store
 var firstAndPike = new Store ('1st and Pike', 'firstAndPike', 23, 65, 6.3, [], []);
@@ -133,17 +140,15 @@ var seattleCenter = new Store('Seattle Center', 'seattleCenter', 11, 38,3.7, [],
 var capitolHill = new Store('Capitol Hill', 'capitolHill', 20, 38, 2.3, [], []);
 var alki = new Store('Alki', 'alki', 2, 16, 4.6, [], []);
 
-// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-// Store the results for each location in a separate array as a property of the object representing that location
-// Store the total number of cookies for each store in the objects for each location
+// Calculate the simulated amounts of cookies purchased
 firstAndPike.SimulatedNumberOfCookies();
 seaTac.SimulatedNumberOfCookies();
 seattleCenter.SimulatedNumberOfCookies();
 capitolHill.SimulatedNumberOfCookies();
 alki.SimulatedNumberOfCookies();
 
-
 createTableHeader();
+
 //Render table data
 firstAndPike.render();
 seaTac.render();
@@ -151,33 +156,5 @@ seattleCenter.render();
 capitolHill.render();
 alki.render();
 
-
-for(i=1; i< HOURS.length + 2; i++){
-  if (i > 1 ){cookiesByHour.push(hourSum);
-  }
-  hourSum = 0;
-  console.log(cookiesByHour);
-  for (j=1; j < hourlyTotalForAllLocations.length + 1; j++){
-    hourTotal = document.getElementById("storeTable").rows[j].cells[i].innerHTML
-    hourSum = hourTotal * 1 + hourSum;
-  }
-}
-
+calculateHourlyTotals();
 createTableFooter();
-
-
-// console.log(hourlyTotalForAllLocations.length + ' length of hour array');
-// for (i=0; i < hourlyTotalForAllLocations.length; i++){
-//   var a = hourlyTotalForAllLocations[i];
-//   for (j=0; j < HOURS.length; j++){
-//     var c = a[j];
-//   }
-// }
-// console.log(hourlyTotalForAllLocations);
-// var hourSum = 0;
-
-
-// var hourTotal = document.getElementById("storeTable").rows[1].cells[3].innerHTML
-
-// console.log(hourTotal);
-
